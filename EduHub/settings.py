401ecd85d5341b4644b856b834232b891d2bba42
sina rasouli <sinarasouli79@gmail.com>
2024-04-22 03:52:45 +0000

    add subpath

--- EduHub/settings.py
+++ EduHub/settings.py
@@ -106,7 +106,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-URL_PREFIX="/eduhub"
 STATIC_URL = "static/"
 
 # Default primary key field type
