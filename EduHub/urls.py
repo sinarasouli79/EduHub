75b51f81642b4d5153b9639910ba7b5804c8dc57
sina rasouli <sinarasouli79@gmail.com>
2024-04-22 03:50:38 +0000

    add subpath

--- EduHub/urls.py
+++ EduHub/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-
+from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
