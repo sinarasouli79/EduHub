73e5db657f36484a4e7d9656763b408c31fc60d8
sina rasouli <sinarasouli79@gmail.com>
2024-04-28 16:41:13 +0330

    add static root

--- EduHub/settings.py
+++ EduHub/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "static/"
-
+STATIC_ROOT = "/var/www/eduhub/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
