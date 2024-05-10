9a9a0e3ad05389bddd26460585741ada364c70c0
sina rasouli <sinarasouli79@gmail.com>
2024-05-10 22:21:33 +0330

    resolve merge conflict

--- EduHub/urls.py
+++ EduHub/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
-<<<<<<< Updated upstream
-from django.urls import path
-urlpatterns = [
-    path('eduhub/admin/', admin.site.urls),
-]
-=======
 from django.urls import include, path
+
 
 urlpatterns = [
     path("eduhub/admin/", admin.site.urls),
@@ -30,4 +27,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
->>>>>>> Stashed changes
